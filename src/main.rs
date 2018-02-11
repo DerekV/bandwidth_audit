482bca108ff39d87d193735e5c440c1fab2665e2
Derek VerLee <dverlee@resolutebi.com>
2018-02-11 18:36:19 -0500

    Initial stab at counting totals by IP pair

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+
+extern crate pcap;
+extern crate packet;
+
+use std::env;
+use std::collections::HashMap;
+use std::path::Path;
+use pcap::Capture;
+use packet::ip::v4::Packet;
+
+
+fn main() {
+    let args:Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let input_file_path = Path::new(filename);
+    let mut cap = Capture::from_file(input_file_path).unwrap();
+
+    let mut linkmap = HashMap::new();
+
+
+    while let Ok(packet) = cap.next() {
+
+        // TODO this assumes IPV4 packets in pcap
+        // will often not be the case
+        // we need to review the Capture global headers for the link type
+        let ip_pkt = Packet::new(packet.data).unwrap();
+
+        let link = (ip_pkt.source(),ip_pkt.destination());
+        let total = linkmap.entry(link).or_insert(0);
+        *total += packet.header.len;
+
+        //  println!("{:?} src:{:?} dst:{:?}",ip_pkt.protocol(),ip_pkt.source(), ip_pkt.destination());
+
+    }
+
+    let mut totals_vec: Vec<(&(std::net::Ipv4Addr,std::net::Ipv4Addr),&u32)> = linkmap.iter().collect();
+    totals_vec.sort_by(|a, b| b.1.cmp(a.1));
+
+    for &(link, total) in &totals_vec {
+        println!("{:?}: {:?}", link, total);
+    }
+}
